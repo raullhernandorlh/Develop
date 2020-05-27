eaf3f17c4ac7762076adda419ab36662d1eb50f8
raullopezhernando <raullopezhernandorlh@gmail.com>
2020-05-27 11:06:08 +0200

    Carpeta "EjerciciosExpress" - Trabajando con preprocesado Jade

--- EjerciciosExpress/TemplatesEngine/routes/index.js
+++ EjerciciosExpress/TemplatesEngine/routes/index.js
@@ -8,16 +8,32 @@ function jade (req,res,next)
     let locals = {
         title:   'Jade',
         link:    'http://jade-lang.com/',
+        description:'EJS limpia el HTML del Javascript con plantillas del lado cliente.'
+            
+                    
+    }
+    res.render('index',locals)
+}
+
+function ejs (req,res,next)
+{
+    let locals = {
+        title:   'EJS',
+        link:    'http://www.embeddedjs.com/',
         description:  'Jade es un sistema de plantillas inspirado en JavaScript y en Haml para utilizarse en Node.js'
                     
     }
     res.render('index',locals)
 } 
+
+
 router
     .get('/', (req,res) =>{
     res.end ('Terminamos la configuracion de nuestra primera APP en Express')
     })
 
     .get('/jade',jade)
+    .get('/ejs',ejs)
+
 
 module.exports = router
